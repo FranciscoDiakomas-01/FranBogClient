6f40c462a713f49467f5c0067a0ad8f68e9d8f4a
Francisco <franciscodiakoma@gmail.com>
2024-11-04 22:39:32 +0100

    loading

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Main from './pages/main/index.jsx';
 import Posts from './pages/posts/index.jsx';
 import About from './pages/about/index.jsx';
 import PostDetails from './pages/postDetails/index.jsx';
+import Login from './pages/login/index.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -30,6 +31,11 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/login",
+    element : <Login/>
+    
+  }
 ]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,21 @@
 
-
+import './index.css'
 export default function Login() {
     return (
-    <main>
-        
-    </main>
+      <main id="login">
+        <article></article>
+        <article></article>
+        <form>
+          <h1>Bem Vindo De Volta</h1>
+          <label>Email</label>
+          <input type="email" required placeholder="exemple@gmail.com" />
+          <label>Senha</label>
+          <input type="password" placeholder="Senha" required />
+          <div>
+            <button type="submit">Entrar</button>
+            <button type="button">Criar Conta</button>
+          </div>
+        </form>
+      </main>
     );
 }
